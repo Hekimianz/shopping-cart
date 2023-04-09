c339e86e2c2d1b5edd3c4b87c47092af6112094a
Aram Hekimian <aramhek121@gmail.com>
2023-04-09 15:17:10 -0600

    Finished mobile design for home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
 function App() {
-  return;
+  return (
+    <div>
+      <Home />
+      <footer>
+        <span>Made by Aram Hekimian</span>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Home.css";
+import logo from "../assets/cover.png";
+import mountain from "../assets/mountain.jpg";
+import models1 from "../assets/models1.jpg";
+import models2 from "../assets/models2.jpg";
+
+export default function Home() {
+  return (
+    <div id="home-cont">
+      <nav id="home-nav-cont">
+        <img id="nav-logo" src={logo} alt="black sky logo" />
+        <div id="nav-links-cont">
+          <a className="nav-link" href="#">
+            Home
+          </a>
+          <a className="nav-link" href="#">
+            Shop
+          </a>
+          <a className="nav-link" href="#">
+            Contact
+          </a>
+        </div>
+      </nav>
+      <section id="home-main-cont">
+        <div id="main-top-cont">
+          <div id="main-top-left">
+            <h1>
+              Explore The <span className="heading-highlight">Unknown</span>
+            </h1>
+            <button>SHOP</button>
+          </div>
+          <img id="mountain" src={mountain} alt="mountain with white halo" />
+        </div>
+        <div id="main-mid-cont">
+          <img id="models1" src={models1} alt="two models" />
+          <p id="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim
+            tortor at auctor urna.{" "}
+          </p>
+        </div>
+        <div id="main-bot-cont">
+          <img id="gallery" src={models2} />
+        </div>
+      </section>
+    </div>
+  );
+}
