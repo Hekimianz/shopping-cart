38267bda6c81b8ded01fc5a82137040e5b978e23
Aram Hekimian <aramhek121@gmail.com>
2023-04-25 19:50:54 -0600

    mend

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./components/Shop";
 import Home from "./components/Home";
 
 export default function RouteSwitch() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
