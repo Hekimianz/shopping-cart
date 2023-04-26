1969a8fccd40576463e76b67f11add900dc586aa
Aram Hekimian <aramhek121@gmail.com>
2023-04-25 19:48:22 -0600

    mend

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 
 export default function RouteSwitch() {
   return (
-    <HashRouter basename="/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
